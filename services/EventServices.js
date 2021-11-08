96375a739e7a30160edeaace40cb109522ae1b8a
Atahan <atahansezgin39@gmail.com>
2021-11-08 22:47:04 +0300

    event services updated

--- services/EventServices.js
+++ services/EventServices.js
@@ -3,4 +3,6 @@ import api from '../apiURL.json';
 
 export const getEvents = () => axios.get(api.url+'event/getAll')
 
+export const getEventsByUserId = (id) => axios.get(api.url+`/event/getEventsByUserId?id=${id}`)
+
 export const postEvent = (event,id) => axios.post(api.url+`event/postEvent?id=${id}`,event);
